2a42fed48eb4e7a8de814e1d7be83010047a4347
Shruti Kapoor <shrkapoor@paypal.com>
2019-02-12 21:41:11 -0800

    fixed broken build

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
-var fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-//Reading File Sync
-//var file = process.argv[2];
-//var lines = fs.readFileSync(file).toString().split('\n') -1;
-//console.log(lines);
+const app = express();
 
-//Reading File ASync
-//
-// fs.readFile(file, function(err, data){
-//   if(err){
-//     console.log(err);
-//     return;
-//   }
-//   var lines = data.toString().split('\n').length -1;
-//   console.log(lines);
-// });
-//
+app.use(bodyParser.urlencoded({extended: false}));
 
-//Filtetered LS
-var path = process.argv[2];
-var extension = process.argv[3];
-fs.readdir(path, function(err,files){
-  if(err) return console.log(err);
-  var filesWithPath = files.map((file) => {
-    if(file.split('.')[1] === extension ) console.log(file);
-  });
-})
+app.use('/home', (req, res, next) => {
+    res.send('<h1> Welcome to my home page </h1>');
+});
+
+app.use((req, res, next) => {
+   console.log('Listening to Request');
+   res.status(404).send('<h1> Page not found</h1>');
+});
+app.listen(3000);
