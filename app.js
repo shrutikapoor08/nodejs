7e6c2fc0874c82da9a6af34040621960f5adffa1
Shruti Kapoor <shrkapoor@paypal.com>
2019-02-12 22:48:37 -0800

    end of day commit

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/home', (req, res, next) => {
-    res.send('<h1> Welcome to my home page </h1>');
+    res.sendFile(path.join(__dirname,'views/shop.html'))
 });
 
 app.use((req, res, next) => {
    console.log('Listening to Request');
-   res.status(404).send('<h1> Page not found</h1>');
+   res.status(404).sendFile(path .join(__dirname, 'views/404.html'))
 });
 app.listen(3000);
