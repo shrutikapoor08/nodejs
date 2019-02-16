91e56cc4642ada93550910adfbf088ba97d56878
Shruti Kapoor <shrkapoor@paypal.com>
2019-02-16 14:00:55 -0800

    sending cookies.

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res) => {
+app.use('/',(req, res) => {
    console.log('Listening to Request');
-   res.status(404).sendFile(path .join(__dirname, 'views/404.html'))
+   res.setHeader('Set-Cookie', 'loggedIn=true;' );
+   res.send();
+   // res.status(404).sendFile(path .join(__dirname, 'views/404.html'));
 });
+
+
 app.listen(3000);
